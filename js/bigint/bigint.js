88184709380ba90c0c720c53d45deaccb161ad7b
qubits <ameet@siglabs.com>
2019-06-13 23:36:43 -0700

    add bigint

---
+++ js/bigint/bigint.js
@@ -0,0 +1,8 @@
+const num = BigInt(0xFFFFFFFF);
+const val = parseInt(num);
+const val64 = 0xFFFFFFFFFFFF;
+const val32 = 0xFFFFFFFF;
+console.log(val);
+console.log(val64);
+console.log(val32);
+console.log(num << 1n)
