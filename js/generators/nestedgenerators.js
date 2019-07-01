e07613b2d5e278a088890f1b6fb3a2a901b843b6
qubits <ameet@siglabs.com>
2019-07-01 16:45:09 -0700

    update nestedgenerators

--- js/generators/nestedgenerators.js
+++ js/generators/nestedgenerators.js
@@ -1,16 +1,26 @@
+const _ = require('lodash');
 function* risingedge(clk) {
     yield 1;
     yield 2;
 }
 
-function* driver(clk) {
-    yield* risingedge(clk);
-    yield 3;
+function* driver(clk, count) {
+    for(let i of _.range(count))
+	yield* risingedge(clk);
+    //yield 3;
 }
 
-x = driver(0);
+x = driver(0,5);
+console.log(x.next());
+console.log(x.next());
+console.log(x.next());
+console.log(x.next());
 console.log(x.next());
 console.log(x.next());
 console.log(x.next());
 console.log(x.next());
 
+console.log(x.next());
+console.log(x.next());
+console.log(x.next());
+console.log(x.next());
